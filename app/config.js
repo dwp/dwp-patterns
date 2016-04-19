64922769ce80f5cc1871aef00dfa87d6975400e7
Ed209uk <eddie@shannonigansdesign.com>
2016-04-19 12:10:12 +0100

    Updated page title

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "DWP design patterns",
+  serviceName: "Design patterns",
 
   // Default port that prototype runs on
   port: '3000',
