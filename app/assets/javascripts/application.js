7a524fd4743718fc587c704d2ce6484f236d1960
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-04-25 15:58:42 +0100

    code cleaning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,13 @@
 function ShowHideContent() {
   var self = this;
 
-  self.escapeElementName = function(str) {
-    result = str.replace('[', '\\[').replace(']', '\\]')
-    return(result);
+  self.escapeElementName = function (str) {
+    var result = str.replace('[', '\\[').replace(']', '\\]');
+    return result;
   };
 
   self.showHideRadioToggledContent = function () {
     $(".block-label input[type='radio']").each(function () {
-
       var $radio = $(this);
       var $radioGroupName = $radio.attr('name');
       var $radioLabel = $radio.parent('label');
@@ -19,12 +18,10 @@ function ShowHideContent() {
 
       // If the data-target attribute is defined
       if (dataTarget) {
-
         // Set aria-controls
         $radio.attr('aria-controls', dataTarget);
 
         $radio.on('click', function () {
-
           // Select radio buttons in the same group
           $radio.closest('form').find(".block-label input[name=" + self.escapeElementName($radioGroupName) + "]").each(function () {
             var $this = $(this);
@@ -44,18 +41,14 @@ function ShowHideContent() {
           // Set aria-expanded and aria-hidden for clicked radio
           $radio.attr('aria-expanded', 'true');
           $dataTarget.attr('aria-hidden', 'false');
-
         });
-
       } else {
         // If the data-target attribute is undefined for a radio button,
         // hide visible data-target content for radio buttons in the same group
 
         $radio.on('click', function () {
-
           // Select radio buttons in the same group
           $(".block-label input[name=" + self.escapeElementName($radioGroupName) + "]").each(function () {
-
             var groupDataTarget = $(this).parent('label').attr('data-target');
             var $groupDataTarget = $('#' + groupDataTarget);
 
@@ -65,16 +58,12 @@ function ShowHideContent() {
             $(this).attr('aria-expanded', 'false');
             $groupDataTarget.attr('aria-hidden', 'true');
           });
-
         });
       }
-
     });
-  }
+  };
   self.showHideCheckboxToggledContent = function () {
-
-    $(".block-label input[type='checkbox']").each(function() {
-
+    $(".block-label input[type='checkbox']").each(function () {
       var $checkbox = $(this);
       var $checkboxLabel = $(this).parent();
 
@@ -84,35 +73,30 @@ function ShowHideContent() {
 
       // If the data-target attribute is defined
       if (typeof $dataTarget !== 'undefined' && $dataTarget !== false) {
-
         // Set aria-controls
         $checkbox.attr('aria-controls', $dataTarget);
 
         // Set aria-expanded and aria-hidden
         $checkbox.attr('aria-expanded', 'false');
-        $('#'+$dataTarget).attr('aria-hidden', 'true');
+        $('#' + $dataTarget).attr('aria-hidden', 'true');
 
         // For checkboxes revealing hidden content
-        $checkbox.on('click', function() {
-
-          var state = $(this).attr('aria-expanded') === 'false' ? true : false;
+        $checkbox.on('click', function () {
+          var state = $(this).attr('aria-expanded') === 'false';
 
           // Toggle hidden content
-          $('#'+$dataTarget).toggle();
+          $('#' + $dataTarget).toggle();
 
           // Update aria-expanded and aria-hidden attributes
           $(this).attr('aria-expanded', state);
-          $('#'+$dataTarget).attr('aria-hidden', !state);
-
+          $('#' + $dataTarget).attr('aria-hidden', !state);
         });
       }
-
     });
-  }
+  };
 }
 
-$(document).ready(function() {
-
+$(document).ready(function () {
   // Use GOV.UK selection-buttons.js to set selected
   // and focused states for block labels
   var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']");
@@ -123,5 +107,4 @@ $(document).ready(function() {
   var toggleContent = new ShowHideContent();
   toggleContent.showHideRadioToggledContent();
   toggleContent.showHideCheckboxToggledContent();
-
 });
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,13 +4,12 @@ function ShowHideContent() {
   var self = this;
 
   self.escapeElementName = function (str) {
-    result = str.replace('[', '\\[').replace(']', '\\]');
+    var result = str.replace('[', '\\[').replace(']', '\\]');
     return result;
   };
 
   self.showHideRadioToggledContent = function () {
     $(".block-label input[type='radio']").each(function () {
-
       var $radio = $(this);
       var $radioGroupName = $radio.attr('name');
       var $radioLabel = $radio.parent('label');
@@ -21,12 +20,10 @@ function ShowHideContent() {
 
       // If the data-target attribute is defined
       if (dataTarget) {
-
         // Set aria-controls
         $radio.attr('aria-controls', dataTarget);
 
         $radio.on('click', function () {
-
           // Select radio buttons in the same group
           $radio.closest('form').find(".block-label input[name=" + self.escapeElementName($radioGroupName) + "]").each(function () {
             var $this = $(this);
@@ -52,10 +49,8 @@ function ShowHideContent() {
         // hide visible data-target content for radio buttons in the same group
 
         $radio.on('click', function () {
-
           // Select radio buttons in the same group
           $(".block-label input[name=" + self.escapeElementName($radioGroupName) + "]").each(function () {
-
             var groupDataTarget = $(this).parent('label').attr('data-target');
             var $groupDataTarget = $('#' + groupDataTarget);
 
@@ -70,9 +65,7 @@ function ShowHideContent() {
     });
   };
   self.showHideCheckboxToggledContent = function () {
-
     $(".block-label input[type='checkbox']").each(function () {
-
       var $checkbox = $(this);
       var $checkboxLabel = $(this).parent();
 
@@ -82,7 +75,6 @@ function ShowHideContent() {
 
       // If the data-target attribute is defined
       if (typeof $dataTarget !== 'undefined' && $dataTarget !== false) {
-
         // Set aria-controls
         $checkbox.attr('aria-controls', $dataTarget);
 
@@ -92,8 +84,7 @@ function ShowHideContent() {
 
         // For checkboxes revealing hidden content
         $checkbox.on('click', function () {
-
-          var state = $(this).attr('aria-expanded') === 'false' ? true : false;
+          var state = $(this).attr('aria-expanded') === 'false';
 
           // Toggle hidden content
           $('#' + $dataTarget).toggle();
@@ -108,7 +99,6 @@ function ShowHideContent() {
 }
 
 $(document).ready(function () {
-
   // Use GOV.UK selection-buttons.js to set selected
   // and focused states for block labels
   var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']");
