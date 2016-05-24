870f49a63d28c3f0ef3b85445e3c647f04f5d91a
charlesrt <charlesrt.digitaldwp@gmail.com>
2016-05-24 16:58:59 +0100

    Merge elements and patterns

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Design patterns",
+  serviceName: "Design Manual",
 
   // Default port that prototype runs on
   port: '3000',
