0eaffe5a074f45d8703218f578af9fb148b202dc
Paul Michael Smith <github@airfront.co.uk>
2016-07-19 13:47:05 +0100

    Feature/improve patterns (#15)
    * updates patterns layouts to improve workflow
    
    * removes basic example
    
    * removes lorem copy from a pattern
    
    * remove content for basic filler within example template

--- app/filters.js
+++ app/filters.js
@@ -1,13 +1,11 @@
-var nunjucks = require('nunjucks'),
+var _ = require('lodash'),
     markdown = require('nunjucks-markdown'),
     marked = require('marked');
 
 module.exports = function(env) {
 
   var nunjucksSafe = env.getFilter('safe');
-
   markdown.register(env, marked);
-
   /**
    * Instantiate object used to store the methods registered as a
    * 'filter' (of the same name) within nunjucks. You can override
@@ -46,27 +44,80 @@ module.exports = function(env) {
     documentation.
 
   ------------------------------------------------------------------ */
-
+  
   /**
-   * uses marked library to convert passed value from markdown to HTML
-   * @method markdown
-   * @param  {String} value     the block/string to be converted from markdown
-   * @param  {Boolean} stripPara flag to strip blank paragraphs
-   * @return {String}           converted string of markup
+   * sets globally accessible variable within nunjucks context
+   * @method setGlobal
+   * @param  {*}  val  the value you want to be global
+   * @param  {String}  key  the key/variable name you would like the access the value by
    */
-  filters.markdown = function markdown(value, stripPara) {
-    var result;
-    stripPara = stripPara !== false;
-    try {
-      result = marked(value,{ gfm: false, tables: true, breaks: true, pedantic: true, sanitize: true, smartLists: true, smartypants: true}).trim();
-      if (stripPara) { result = result.replace(/^<p>|<\/p>$/g, ''); }
-      return nunjucksSafe(result);
-    } catch (e) {
-      console.error('Error processing markdown:', e);
-      return value;
+  filters.setGlobal = function setGlobal(val, key) {
+    env.addGlobal(key, nunjucksSafe(val));
+  }
+  
+  /**
+   * returns a filtered collection where the value of the key is not empty
+   * @method filterByEmpty
+   * @param  {Array} list the array/object being filtered
+   * @param  {String} key  the key being checked is not empty
+   * @return {Array}      the filtered collection minus those with empty values in the key provided
+   */
+  filters.filterByEmpty = function filterByEmpty(collection,key) {
+    if (_.isObjectLike(collection)) {
+      return _.filter(collection, function(o) { return !_.isEmpty(o[key]); });
     }
   }
+  
+  /**
+   * translate characters in a string
+   * @param  {String} s  the string to translate
+   * @param  {String} ss the substring to replace
+   * @param  {String} r  the replacee string
+   * @param  {String} f  regex flags, 'g' by default
+   * @return {String}    a translated string
+   */
+  filters.trC = function trC(s, ss, r, f) {
+  	return (s||'').replace(new RegExp(ss, typeof f === 'string' ? f : 'g'), r);
+  };
+  
+  /**
+   * deep merge that supports concating arrays & strings.
+   * @param  {Object} o1           a plain object to merge
+   * @param  {Object} o2           a plain object to merge
+   * @param  {Boolean} mergeStrings will merge strings together if true
+   * @return {Object}              the resulting merged object
+   */
+  filters.deeperMerge = function deeperMerge(o1, o2, mergeStrings) {
 
+  	mergeStrings = typeof mergeStrings !== undefined ? mergeStrings : false;
+
+  	// exit conditions
+  	if      ( ! o1 && ! o2 )          { return; }
+  	else if ( ! _.isPlainObject(o1) ) { return o2; }
+  	else if ( ! _.isPlainObject(o2) ) { return o1; }
+
+  	return _
+  		.union(Object.keys(o1), Object.keys(o2))
+  		.map(function(k) {
+  			return [k, (
+  				( typeof o1[k] === 'string' && typeof o2[k] === 'string' ) ? ( mergeStrings ? o1[k] + o2[k] : o2[k] ) :
+  				( _.isPlainObject(o1[k]) || _.isPlainObject(o2[k]) ) ? deeperMerge(o1[k], o2[k], mergeStrings) :
+  				( _.isArray(o1[k]) && _.isArray(o2[k]) ) ? o1[k].concat(o2[k]) :
+  				( o1[k] && !o2[k] ) ? o1[k] : o2[k]
+  			)];
+  		})
+  		.reduce(function(a, b) { return (a[b[0]] = b[1], a) }, {});
+  };
+  
+  /**
+   * lodash methods exposed as filters
+   * see documentation here: https://lodash.com/docs
+   */
+  filters.unescape = _.unescape;
+  filters.filter = _.filter;
+  filters.isEmpty = _.isEmpty;
+  filters.merge = _.merge;
+  
   /* ------------------------------------------------------------------
     keep the following line to return your filters to the app
   ------------------------------------------------------------------ */
